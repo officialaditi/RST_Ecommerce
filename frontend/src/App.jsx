f8d0364c657a4d9f1021c9ba4c62546fc74f8dc9
Aditi Gaikwad <aditigaikwad0110@gmail.com>
2025-11-12 12:29:18 +0530

    Step 2 : Tailwind installation and setup done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,8 @@
-
 const App = () => {
-return (
-  <div>
-    <h1>hello world</h1>
-  </div>
-)
-}
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        </div>
+    );
+};
 export default App;
