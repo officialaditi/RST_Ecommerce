7bee5e86fbbcc70ca37ceadf2c18f9613aae1fb8
Aditi Gaikwad <aditigaikwad0110@gmail.com>
2025-11-18 16:16:56 +0530

    Step 18: fetching products from database done

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import products from './data/products.js'
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import colors from 'colors'
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
 
@@ -15,14 +15,8 @@ app.get('/', (req, res) => {
       res.send('hello world')
 });
 
-app.get('/api/products', (req, res) => {
-      res.json(products);
-})
 
-app.get('/api/products/:id', (req, res) => {
-      const product = products.find((prod) => prod._id === req.params.id);
-      res.send(product)
-})
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 3000;
 
