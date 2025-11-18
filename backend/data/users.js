c88c3dc9efdae1856a767788a1a5e121757ca1e2
Aditi Gaikwad <aditigaikwad0110@gmail.com>
2025-11-18 11:34:56 +0530

    Step 16: Prepare sample data for backend data seeding users Done

---
+++ backend/data/users.js
@@ -0,0 +1,25 @@
+import bcrypt from "bcryptjs";
+
+const users = [
+      { 
+            name: 'Admin',
+            email: 'admin@example.com',
+            password: bcrypt.hashSync('12345', 10), // 10 is salt
+            isAdmin: true
+      },
+      {
+            name: 'John Doe',
+            email: 'john@example.com',
+            password: bcrypt.hashSync('12345', 10),
+            isAdmin: false
+      }, 
+      {
+            name: 'Mary Jonas',
+            email: 'mary@example.com',
+            password: bcrypt.hashSync('12345', 10),
+            isAdmin: false
+      }
+]
+
+console.log(users);
+export default users;
