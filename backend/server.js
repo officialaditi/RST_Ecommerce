5dede17b58d521c91c0eef1a8b1040994443c34c
Aditi Gaikwad <aditigaikwad0110@gmail.com>
2025-11-18 16:26:59 +0530

    Step 19 and 20: custom error handler, set into the root server.js folder Done

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js';
+import { errorHandler, notFound } from './middlewares/errorMiddleware.js';
 
 dotenv.config();
 
@@ -17,6 +18,11 @@ app.get('/', (req, res) => {
 
 
 app.use('/api/products', productRoutes);
+
+// only run when routes have an issue
+app.use(notFound);
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT || 3000;
 
